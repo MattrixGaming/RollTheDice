c008d120b7a9d79185506215bccac1f56d7e36fd
MattrixGaming <mrask1113@gmail.com>
2021-10-14 18:32:27 -0400

    Initial commit

---
+++ src/com/RollTheDice/rollthedice/Dice.java
@@ -0,0 +1,5 @@
+package com.RollTheDice.rollthedice;
+
+public class Dice {
+
+}
---
+++ src/com/RollTheDice/rollthedice/Main.java
@@ -0,0 +1,57 @@
+package com.RollTheDice.rollthedice;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Initialization
+
+        int balance = 1000;
+
+        betCalc(balance);
+        System.out.println(balance);
+        //rollDice(rolled);
+
+
+    }
+    public static int betCalc(int bal){
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int multi;
+        int r;
+        int betAmount = 1;
+
+        if(betAmount >0 && betAmount <= bal){
+            System.out.println("Enter Bet amount (Bank: " + bal + "): ");
+            betAmount = scan.nextInt();
+            int dice1 = rand.nextInt(6) + 1;
+            int dice2 = rand.nextInt(6) + 1;
+            int diceSum = dice1 + dice2;
+            if(diceSum == 7){
+                multi = 2;
+            }else if(diceSum == 11){
+                multi = 1;
+            }else if(diceSum == 2){
+                multi = -1000;
+            }else{
+                multi = 0;
+            }
+            r = betAmount * multi;
+            bal += r;
+            System.out.println("Remaining Balance: $" + bal);
+        }else if(betAmount <=0){
+            System.out.println("You have $" + bal);
+            System.exit(0);
+        }
+        return bal;
+    }
+//    public static int rollDice(int r) {
+//
+//        return diceSum;
+//    }
+//    public static int multi(int m){
+//        int multi;
+//
+//        return multi;
+//    }
+}
